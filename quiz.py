351d2a5f01c0dc1555caa6e7858c24461fede4bb
coopersigrist <csigrist@umass.edu>
2024-11-13 15:39:49 -0500

    added a quiz

---
+++ quiz.py
@@ -0,0 +1,6 @@
+'''
+
+To get credit for your quiz you need to implement a class that could be used for your final project and upload a screenshot of your
+class definition. You must come up with your own outline and you should define an __init__ function too.
+
+'''
