300fef63151f14045bee350e051ba1dfb6607c91
csigrist@umass.edu <csigrist@umass.edu>
2024-11-13 18:06:33 -0500

    added bonus2

---
+++ bonus2.py
@@ -0,0 +1,16 @@
+'''
+
+Worth 2 stars
+
+Using a similar idea as bonus 1 (objects of the same class being set to attributes of the class) create a class for a Binary Tree (https://www.geeksforgeeks.org/binary-tree-data-structure/)
+The class needs to have an initialization that can take an unsorted array and turn it into a tree, as well as methods that can do the following:
+
+1. Add an element to the tree
+2. Delete an element from the tree by value
+3. balance the tree (equalize the size of the left and right sizes as much as possible)
+4. Give a sorted list of the elements
+
+All implementation details and testing are left up to you
+
+'''
+
